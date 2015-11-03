456d53c92fc07a6dbbe75715f4847f35e2054fe7
jiraspiom <jiraspiom@yahoo.com.br>
2015-11-03 15:05:51 -0200

    adicionado a classe cliente vazia

---
+++ testeVB2013Git/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeVB2013Git
+{
+    class Cliente
+    {
+    }
+}
